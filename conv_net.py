efc446f40fee051ee68554204d2b758c7808d48e
Gary <chanokin@gmail.com>
2020-10-28 12:34:35 +0000

    start to make conv connector

--- conv_net.py
+++ conv_net.py
@@ -1,22 +1,29 @@
 import numpy as np
 import spynnaker8 as sim
 
-shape = np.array([5, 5], dtype='int')  # h, w
-n_input = np.prod(shape, dtype='int')
-stride = np.array([1, 1], dtype='int')  # h, w
-k_shape = np.array([3, 3], dtype='int')
-
-n_out = np.prod(np.floor((shape - k_shape) / stride) - 1, dtype='int')
-sim.setup(timestep=1.)
-
+shape = np.array([5, 5], dtype='int32')  # h, w
+n_input = np.prod(shape, dtype='int32')
+stride = np.array([1, 1], dtype='int32')  # h, w
+k_shape = np.array([3, 3], dtype='int32')
+shape_out = (np.floor((shape - k_shape) / stride) - 1).astype('int32')
+n_out = np.prod(shape_out, dtype='int32')
 vline = [[10.] if (idx % shape[1]) == (shape[1] // 2) else []
          for idx in range(n_input)]
+
+ws = np.zeros(k_shape)
+ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 1.
+
+run_time = 50.
+
+sim.setup(timestep=1.)
 
 src = sim.Population(n_input, sim.SpikeSourceArray,
                      {'spike_times': vline}, label='input spikes')
 
 dst = sim.Population(n_out, sim.IF_curr_exp_conv, {})
 
-conn = sim.KernelConnector()
+conn = sim.ConvolutionConnector(shape, shape_out, k_shape)
 
-prj = sim.Projection(src, dst, sim.KernelConnector)
+prj = sim.Projection(src, dst, conn)
+
+sim.run(run_time)
--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -13,6 +13,7 @@ fi
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/spinnaker_tools.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/spinn_common.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/ybug.git
+git clone -b $BRANCH https://github.com/SpiNNakerManchester/spalloc.git
 
 # Clone sPyNNaker and requirements
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNUtils.git
@@ -21,13 +22,15 @@ git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNMachine.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNMan.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/PACMAN.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon.git
-git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNaker.git
+#git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNaker.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNStorageHandlers.git
 git clone -b $BRANCH https://github.com/SpiNNakerManchester/SpiNNakerGraphFrontEnd.git
 # Clone extra modules
-git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNakerExtraModelsPlugin.git
-git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNakerExternalDevicesPlugin.git
-git clone -b $BRANCH https://github.com/SpiNNakerManchester/PyNNExamples.git
+#git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNakerExtraModelsPlugin.git
+#git clone -b $BRANCH https://github.com/SpiNNakerManchester/sPyNNakerExternalDevicesPlugin.git
+#git clone -b $BRANCH https://github.com/SpiNNakerManchester/PyNNExamples.git
+
+exit
 
 #---------------------------------------
 # Setup virtualenv
@@ -52,6 +55,8 @@ python setup.py develop --no-deps
 cd ../DataSpecification
 python setup.py develop --no-deps
 
+cd ../spalloc
+python setup.py develop --no-deps
 
 cd ../SpiNNMan
 python setup.py develop --no-deps
