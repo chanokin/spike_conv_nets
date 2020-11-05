c4087050b18e3c067bcb40e00197acc4b4aac560
chanokin <chanokin@gmail.com>
2020-11-05 19:59:00 +0000

    need to figure out how events are associated slices to filter per connector

--- conv_net.py
+++ conv_net.py
@@ -2,17 +2,19 @@ import numpy as np
 import spynnaker8 as sim
 from pyNN.space import Grid2D
 
+sim.SpikeSourceArray.set_model_max_atoms_per_core( n_atoms=10)
+
 shape = np.array([5, 5], dtype='int32')  # h, w
 n_input = np.prod(shape, dtype='int32')
 stride = np.array([1, 1], dtype='int32')  # h, w
 k_shape = np.array([3, 3], dtype='int32')
-shape_out = (np.floor((shape - k_shape) / stride) - 1).astype('int32')
-n_out = np.prod(shape_out, dtype='int32')
 vline = [[10.] if (idx % shape[1]) == (shape[1] // 2) else []
          for idx in range(n_input)]
 
 ws = np.zeros(k_shape)
-ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 1.
+ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 2.
+ws[:, :k_shape[1]//2] = -1.
+ws[:, k_shape[1]//2+1:] = -1.
 
 run_time = 50.
 
@@ -21,11 +23,11 @@ sim.setup(timestep=1.)
 src = sim.Population(n_input, sim.SpikeSourceArray,
                      {'spike_times': vline}, label='input spikes')
 
-conn = sim.ConvolutionConnector(shape, k_shape, strides=stride)
+conn = sim.ConvolutionConnector(shape, ws, strides=stride)
 shape_out = conn.get_post_shape()
 n_out = np.prod(shape_out, dtype='int32')
 dst = sim.Population(n_out, sim.IF_curr_exp_conv, {})
-
+# syn = sim.StaticSynapse(weight=ws.flatten)
 
 prj = sim.Projection(src, dst, conn)
 
