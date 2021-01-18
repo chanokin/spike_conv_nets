50c727ee2699a5f3cecf0f5c2733690ff8d1fedf
chanokin <chanokin@gmail.com>
2021-01-18 17:04:25 +0000

    reordered state variables so that they are correctly decoded in C
    spike processing almost working

--- conv_net.py
+++ conv_net.py
@@ -2,13 +2,13 @@ import numpy as np
 import spynnaker8 as sim
 from pyNN.space import Grid2D
 
-sim.SpikeSourceArray.set_model_max_atoms_per_core( n_atoms=11)
+sim.SpikeSourceArray.set_model_max_atoms_per_core(n_atoms=11)
 
 shape = np.array([5, 5], dtype='int32')  # h, w
 n_input = np.prod(shape, dtype='int32')
 stride = np.array([1, 1], dtype='int32')  # h, w
 k_shape = np.array([3, 3], dtype='int32')
-vline = [[10.] if (idx % shape[1]) == (shape[1] // 2) or idx == 13 else []
+vline = [[5. + idx%3] if (idx % shape[1]) == (shape[1] // 2) or idx == 13 else []
          for idx in range(n_input)]
 
 for x, i in enumerate(vline):
@@ -17,10 +17,10 @@ for x, i in enumerate(vline):
 
 ws = np.zeros(k_shape)
 ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 2.
-ws[:, k_shape[1]//2] = -1.
-ws[:, k_shape[1]//2+1:] = 5.
+ws[:, k_shape[1]//2] = -1.8
+ws[:, k_shape[1]//2+1:] = 5.5
 
-run_time = 50.
+run_time = 15.
 
 sim.setup(timestep=1.)
 
@@ -31,8 +31,23 @@ conn = sim.ConvolutionConnector(shape, ws, strides=stride)
 shape_out = conn.get_post_shape()
 n_out = np.prod(shape_out, dtype='int32')
 dst = sim.Population(n_out, sim.IF_curr_exp_conv, {})
+dst.record('v')
 # syn = sim.StaticSynapse(weight=ws.flatten)
 
 prj = sim.Projection(src, dst, conn)
 
 sim.run(run_time)
+
+neo = dst.get_data('v')
+v = neo.segments[0].filter(name='v')[0]
+print(v)
+
+sim.end()
+
+import matplotlib.pyplot as plt
+plt.figure()
+for i, vv in enumerate(v.T):
+    plt.plot(vv, label=i)
+plt.legend()
+plt.show()
+
