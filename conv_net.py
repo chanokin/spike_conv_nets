c3be3fe8675cf6cd9d280e885f9d5577fbc9137f
chanokin <chanokin@gmail.com>
2021-01-21 17:36:20 +0000

    fix on neuron_impl_standard.h variable, voltage now seems to follow ok - need to do some work on bit-field part to record/send spikes?

--- conv_net.py
+++ conv_net.py
@@ -8,7 +8,7 @@ shape = np.array([5, 5], dtype='int32')  # h, w
 n_input = np.prod(shape, dtype='int32')
 stride = np.array([1, 1], dtype='int32')  # h, w
 k_shape = np.array([3, 3], dtype='int32')
-vline = [[20. + idx%3] if (idx % shape[1]) == (shape[1] // 2) or idx == 13 else []
+vline = [[20.] if (idx % shape[1]) == (shape[1] // 2) or idx == 13 else []
          for idx in range(n_input)]
 
 for x, i in enumerate(vline):
@@ -18,16 +18,16 @@ for x, i in enumerate(vline):
 ws = np.zeros(k_shape)
 ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 2.
 ws[:, k_shape[1]//2] = -1.8
-ws[:, k_shape[1]//2+1:] = 5.5
+ws[:, k_shape[1]//2+1:] = 1.
 
 print(np.sum(ws))
 # ws[:] = ws / np.sum(ws**2)
 # ws[:] = ws - np.mean(ws)
-ws[ws > 0] /= np.sum( (ws > 0) * ws )
-ws[ws < 0] /= -np.sum( (ws < 0) * ws )
-ws *= 3.
-print(np.sum(ws))
-print(ws)
+# ws[ws > 0] /= np.sum( (ws > 0) * ws )
+# ws[ws < 0] /= -np.sum( (ws < 0) * ws )
+# ws *= 3.
+# print(np.sum(ws))
+# print(ws)
 
 run_time = 50.
 
@@ -42,7 +42,7 @@ n_out = np.prod(shape_out, dtype='int32')
 dst = sim.Population(n_out, sim.IF_curr_exp_conv,
                      {'v_thresh': -60.0,
                       'v_reset': -80.0})
-dst.record('v')
+dst.record(['v', 'spikes'])
 # syn = sim.StaticSynapse(weight=ws.flatten)
 
 prj = sim.Projection(src, dst, conn)
@@ -51,7 +51,9 @@ sim.run(run_time)
 
 neo = dst.get_data('v')
 v = neo.segments[0].filter(name='v')[0]
+spikes = neo.segments[0].spiketrains
 print(v)
+print(spikes)
 
 sim.end()
 
