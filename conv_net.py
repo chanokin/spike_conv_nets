e618e7f94d1abe7a12daf9a8986efcba0f8bccd0
Gary <chanokin@gmail.com>
2021-01-22 12:18:10 +0000

    not reaching every possible post neuron >:(

--- conv_net.py
+++ conv_net.py
@@ -11,15 +11,12 @@ k_shape = np.array([3, 3], dtype='int32')
 vline = [[20.] if (idx % shape[1]) == (shape[1] // 2) or idx == 13 else []
          for idx in range(n_input)]
 
-for x, i in enumerate(vline):
-    if x:
-        print(i, x)
 
-ws = np.zeros(k_shape)
-ws[:, k_shape[1]//2] = np.arange(k_shape[0]) + 2.
-ws[:, k_shape[1]//2] = -1.8
-ws[:, k_shape[1]//2+1:] = 1.
-
+ws = np.arange(np.prod(k_shape), dtype='float').reshape(k_shape)
+# ws[:, k_shape[1]//2] = np.arange(k_shape[1]) + 2.
+ws[:, k_shape[1]//2] *= -0.8
+# ws[:, k_shape[1]//2+1:] = 0.
+print(ws)
 print(np.sum(ws))
 # ws[:] = ws / np.sum(ws**2)
 # ws[:] = ws - np.mean(ws)
@@ -29,6 +26,8 @@ print(np.sum(ws))
 # print(np.sum(ws))
 # print(ws)
 
+
+
 run_time = 50.
 
 sim.setup(timestep=1.)
@@ -37,6 +36,22 @@ src = sim.Population(n_input, sim.SpikeSourceArray,
                      {'spike_times': vline}, label='input spikes')
 
 conn = sim.ConvolutionConnector(shape, ws, strides=stride)
+
+hh, hw = k_shape // 2
+for i, x in enumerate(vline):
+    if len(x):
+        print(i, x)
+        r, c = i // shape[1], i % shape[1]
+        print("pre {}, r {}, c {}".format(i, r, c))
+        postr, postc = conn.pre_as_post(r, c)
+        print("postr {}, postc {}".format(postr, postc))
+        for kr in range(-hh, hh+1):
+            for kc in range(-hw, hw+1):
+                print("row {}, col {}, w {}".format(
+                    postr + kr, postc + kc,
+                    ws[kr + k_shape[0] // 2, kc + k_shape[1] // 2]
+                ))
+
 shape_out = conn.get_post_shape()
 n_out = np.prod(shape_out, dtype='int32')
 dst = sim.Population(n_out, sim.IF_curr_exp_conv,
