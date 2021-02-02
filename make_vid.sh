5e9b564b99934ea95db4cb1430594a3e12d767d6
chanokin <chanokin@gmail.com>
2021-02-02 20:23:28 +0000

    pulse tests seem ok

--- make_vid.sh
+++ make_vid.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-ffmpeg -i sim_output_%010d.png -vcodec mpeg4 -y -r 2 movie.mp4
+#ffmpeg -r 1 -i "test_img_sim_output_*.png" -vcodec mpeg4 -y -r 1 movie.mp4
+cat test_img_sim_output_*.png | ffmpeg -f image2pipe -r 1 -i - -c:v copy -r 2 movie.mp4
