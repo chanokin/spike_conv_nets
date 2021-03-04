b654877a31fd7b596455d9f96e9a17dc2023ac94
chanokin <chanokin@gmail.com>
2021-03-04 22:07:39 +0000

    extracted weights from ml_genn model

--- extract_net_mlgenn.py
+++ extract_net_mlgenn.py
@@ -1,7 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers, datasets
+from ml_genn import Model
+from ml_genn.layers import InputType
+from ml_genn.norm import DataNorm, SpikeNorm
+from ml_genn.utils import parse_arguments, raster_plot
+import numpy as np
+
+
 
 param_trans = {
     'neurons': {
-        'threshold': ('global_params', 'Vthr'),
+        'threshold': ('extra_global_params', 'Vthr'),
+        'shape': 'shape',
+    },
+    'input': {
         'shape': 'shape',
     },
     'conv2d': {
@@ -24,20 +36,24 @@ param_trans = {
 
 
 def get_neuron_params(lyr, translations=param_trans):
-    ft = translations['neurons']
+    k = 'neurons' if lyr.name != 'input' else 'input'
+    ft = translations[k]
     d = {}
-    ns = lyr.neurons
     for k in ft:
+        o = lyr if k == 'shape' else lyr.neurons.nrn
         if isinstance(ft[k], str):
-            d[k] = getattr(ns, ft[k])
+            d[k] = getattr(o, ft[k])
         else:
-            tmp = getattr(ns, ft[k][0])
-            d[k] = tmp[ft[k][1]]
+            tmp = getattr(o, ft[k][0])
+            d[k] = tmp[ft[k][1]].view[0]
     return d
 
 
 def get_connectivity(lyr, translations=param_trans):
-    tp = type(lyr).lower()
+    if lyr.name == 'input':
+        return {}
+
+    tp = str(type(lyr)).lower()
     if 'conv2d' in tp:
         k = 'conv2d'
     else:  # 'dense' in tp:
@@ -53,14 +69,43 @@ def get_connectivity(lyr, translations=param_trans):
             tmp = getattr(ws, ft[k][0])
             d[k] = tmp[ft[k][1]]
 
-    if 'avepool2d' in tp:
+    if 'pool' in tp:
         ft = translations['avepool2d']
+        pd = {}
         for k in ft:
             if isinstance(ft[k], str):
-                d[k] = getattr(ws, ft[k])
+                pd[k] = getattr(ws, ft[k])
             else:
                 tmp = getattr(ws, ft[k][0])
-                d[k] = tmp[ft[k][1]]
-
+                pd[k] = tmp[ft[k][1]]
+        d['pool'] = pd
     return d
 
+
+
+tf_model = models.load_model('simple_cnn_tf_model')
+mlg_model = Model.convert_tf_model(tf_model, input_type='poisson', 
+                connectivity_type='procedural')
+mlg_model.compile(dt=1.0, batch_size=1, rng_seed=0)
+
+print(mlg_model)
+params = {}
+conns = {}
+order = []
+for layer in mlg_model.layers:
+    name = layer.name
+    params[name] = get_neuron_params(layer)
+    conns[name] = get_connectivity(layer)
+    order.append(name)
+
+for n in order:
+    print(n)
+    for p in conns[n]:
+        if p == 'weights':
+            continue
+        print(p, conns[n][p])
+    # print(conns[n])
+    print(params[n])
+
+np.savez_compressed("simple_cnn_network_elements.npz",
+    params=params, conns=conns, order=order)
