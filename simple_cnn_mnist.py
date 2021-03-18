60ec44b4762418bb933199d7755df4dba8c92b3d
chanokin <chanokin@gmail.com>
2021-03-18 22:28:47 +0000

    opening mnist from pypi mnist package and setting it into variable poisson: just the poisson pop keeps falling behind, this is not promising :(

--- simple_cnn_mnist.py
+++ simple_cnn_mnist.py
@@ -1,5 +1,7 @@
 import spynnaker8 as sim
 import numpy as np
+import mnist
+import matplotlib.pyplot as plt
 
 filename = "simple_cnn_network_elements.npz"
 
@@ -9,20 +11,71 @@ order = data['order']
 ml_conns = data['conns'].item()
 ml_param = data['params'].item()
 
-print(data.keys())
+print(list(data.keys()))
 
+test_X = mnist.test_images()
+test_y = mnist.test_labels()
 
+# shape of dataset
+print('X_test:  ' + str(test_X.shape))
+print('Y_test:  ' + str(test_y.shape))
+
+# plotting
+
+for i in range(9):
+    plt.subplot(330 + 1 + i)
+    plt.imshow(test_X[i], cmap=plt.get_cmap('gray'))
+
+plt.show()
+sim.extra_models.SpikeSourcePoissonVariable.set_model_max_atoms_per_core(100)
 sim.setup(timestep=1.)
 
 np.random.seed(13)
-n_neurons = 10
-rates = np.random.randint(0, 20, size=())
-MF_population = sim.Population(num_MF_neurons,  # number of sources
-                               sim.extra_models.SpikeSourcePoissonVariable,  # source type
-                               {'rates': all_mf_rates,
-                                'starts': all_mf_starts,
-                                'durations': all_mf_durations
-                                },  # source spike times
-                               label="MF",
-                               additional_parameters={'seed': 24534}
-                               )
+
+shape_in = np.asarray([28, 28])
+n_in = int(np.prod(shape_in))
+n_digits = 5
+digit_duration = 500.0  # ms
+digit_rate = 5.0  # hz
+in_rates = np.zeros((n_in, n_digits))
+for i in range(n_digits):
+    in_rates[:, i] = test_X[i].flatten()
+
+in_rates *= (digit_rate / in_rates.max())
+in_durations = np.ones((n_in, n_digits)) * digit_duration
+in_starts = np.repeat([np.arange(n_digits) * digit_duration],
+                      n_in, axis=0)
+in_params = {
+    'rates': in_rates,
+    'starts': in_starts,
+    'durations': in_durations
+}
+pops = {
+    'in': sim.Population(
+        n_in,  # number of sources
+        sim.extra_models.SpikeSourcePoissonVariable,  # source type
+        in_params,
+        label='mnist',
+        additional_parameters={'seed': 24534}
+    )
+}
+
+pops['in'].record('spikes')
+
+sim_time = digit_duration * n_digits * 1.1
+
+sim.run(sim_time)
+neo = pops['in'].get_data()
+
+spikes = neo.segments[0].spiketrains
+
+sim.end()
+
+# print(spikes)
+
+plt.figure()
+for i, spks in enumerate(spikes):
+    plt.plot(spks, i * np.ones_like(spks), '.b', markersize=1.)
+plt.show()
+
+
