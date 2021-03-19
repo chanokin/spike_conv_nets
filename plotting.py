4908b14681dffcad453ff72fec6da7c00186334a
Gary <chanokin@gmail.com>
2021-03-19 12:12:56 +0000

    getting ready to test simple_cnn_mnist.py

---
+++ plotting.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def spikes_to_bins(spikes, max_t, t_bin):
+    tbins = np.arange(0, max_t, t_bin)
+    sbins = [[[] for _ in spikes] for _ in tbins]
+    for nidx, spks in enumerate(spikes):
+        ids = np.digitize(np.asarray(spks), tbins)
+        for sidx, bidx in enumerate(ids):
+            sbins[bidx-1][nidx].append(spks[sidx])
+
+    return sbins
+
+def spikes_to_images(spikes, shape, max_t, t_bin):
+    binned = spikes_to_bins(spikes, max_t, t_bin)
+    images = []
+    for bidx, sbin in enumerate(binned):
+        img = np.zeros(shape)
+        for nidx, spks in enumerate(sbin):
+            nspks = len(spks)
+            if nspks:
+                r, c = nidx // shape[1], nidx % shape[1]
+                img[r, c] = nspks
+        images.append(img)
+
+    return images, binned
+
+
--- simple_cnn_mnist.py
+++ simple_cnn_mnist.py
@@ -2,6 +2,7 @@ import spynnaker8 as sim
 import numpy as np
 import mnist
 import matplotlib.pyplot as plt
+import plotting
 
 filename = "simple_cnn_network_elements.npz"
 
@@ -21,22 +22,22 @@ print('X_test:  ' + str(test_X.shape))
 print('Y_test:  ' + str(test_y.shape))
 
 # plotting
+# for i in range(9):
+#     plt.subplot(330 + 1 + i)
+#     plt.imshow(test_X[i], cmap=plt.get_cmap('gray'))
+#
+# plt.show()
 
-for i in range(9):
-    plt.subplot(330 + 1 + i)
-    plt.imshow(test_X[i], cmap=plt.get_cmap('gray'))
-
-plt.show()
-sim.extra_models.SpikeSourcePoissonVariable.set_model_max_atoms_per_core(100)
+# sim.extra_models.SpikeSourcePoissonVariable.set_model_max_atoms_per_core(300)
 sim.setup(timestep=1.)
 
 np.random.seed(13)
 
 shape_in = np.asarray([28, 28])
 n_in = int(np.prod(shape_in))
-n_digits = 5
+n_digits = 9
 digit_duration = 500.0  # ms
-digit_rate = 5.0  # hz
+digit_rate = 10.0  # hz
 in_rates = np.zeros((n_in, n_digits))
 for i in range(n_digits):
     in_rates[:, i] = test_X[i].flatten()
@@ -51,31 +52,100 @@ in_params = {
     'durations': in_durations
 }
 pops = {
-    'in': sim.Population(
+    'input': [sim.Population(  # put into list for ease of connection
         n_in,  # number of sources
         sim.extra_models.SpikeSourcePoissonVariable,  # source type
         in_params,
         label='mnist',
         additional_parameters={'seed': 24534}
-    )
+    )]
 }
 
-pops['in'].record('spikes')
+pops['input'][0].record('spikes')
+
+# ------------------------------------------------------------------- #
+# ------------------------------------------------------------------- #
+def_params = {
+    'v_thresh': 1.,
+    'v_rest': 0.,
+    'v_reset': 0.,
+    'v': 0.,
+    'tau_m': np.round(digit_duration // 3.),
+}
+
+for i, o in enumerate(order):
+    if i == 0:
+        continue
+
+    par = ml_param[o]
+    if 'conv2d' in o.lower():
+        shape = par['shape'][0:2]
+        chans = par['shape'][2]
+        ps = def_params.copy()
+        ps['v_thresh'] = par['threshold']
+        n = int(np.prod(shape))
+        print(o, n, shape, chans)
+        pop = [sim.Population(n, sim.IF_curr_exp_conv, ps,
+                              label="{}_chan_{}".format(o, chan))
+               for chan in range(chans)]
+    elif 'dense' in o.lower():
+        shape = par['shape'][0:2]
+
+        pop = [sim.Population(n, sim.IF_curr_exp_pool_dense, ps,
+                              label="{}_chan".format(o))]
+
+
+    pops[o] = pop
+
+projs = {}
+for i, o in enumerate(order):
+    if i == 0:
+        continue
+    o0 = order[i-1]
+    for prei, pre in enumerate(pops[o0]):
+        pre_shape = ml_param[o0]['shape'][:2]
+        c = ml_conns[o]
+        for posti, post in enumerate(pops[o]):
+            if 'conv2d' in o.lower():
+                wshape = c['shape']
+                print(prei, posti, wshape, c['weights'].shape)
+                strides = c['strides']
+                w = c['weights'][:, :, prei, posti].reshape(wshape)
+                lbl = "{} to {}_{}".format(o0, o, posti)
+                pool_area = c['pool']['shape'] if 'pool' in c else None
+                pool_stride = c['pool']['strides'] if 'pool' in c else None
+                cn = sim.ConvolutionConnector(pre_shape, w, strides=strides,
+                                          pooling=pool_area, pool_stride=pool_stride)
+                prj = sim.Projection(pre, post, cn, label=lbl)
+                projs[lbl] = prj
+            elif 'dense' in o.lower():
+                wshape = c['shape']
+
+
+
 
 sim_time = digit_duration * n_digits * 1.1
 
 sim.run(sim_time)
-neo = pops['in'].get_data()
 
+neo = pops['input'][0].get_data()
 spikes = neo.segments[0].spiketrains
 
 sim.end()
 
-# print(spikes)
+imgs, bins = plotting.spikes_to_images(spikes, shape_in, sim_time,
+                                       digit_duration//2)
+nrows = 3
+nimgs = len(imgs)
+ncols = nimgs // nrows + int(nimgs % nrows > 0)
 
-plt.figure()
-for i, spks in enumerate(spikes):
-    plt.plot(spks, i * np.ones_like(spks), '.b', markersize=1.)
+fig = plt.figure(figsize=(ncols, nrows))
+for i in range(nimgs):
+    ax = plt.subplot(nrows, ncols, i + 1)
+    ax.imshow(imgs[i])
+    ax.set_xticks([])
+    ax.set_yticks([])
+
 plt.show()
 
 
