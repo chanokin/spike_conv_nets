caf77e114ab89246da30c5212e343d2c74e990fb
Gary <chanokin@gmail.com>
2021-03-24 12:47:23 +0000

    debugging first layer, output ids don't seem to match in C and output images

--- plotting.py
+++ plotting.py
@@ -5,9 +5,10 @@ def spikes_to_bins(spikes, max_t, t_bin):
     tbins = np.arange(0, max_t, t_bin)
     sbins = [[[] for _ in spikes] for _ in tbins]
     for nidx, spks in enumerate(spikes):
-        ids = np.digitize(np.asarray(spks), tbins)
-        for sidx, bidx in enumerate(ids):
-            sbins[bidx-1][nidx].append(spks[sidx])
+        if len(spks):
+            ids = np.digitize(np.asarray(spks), tbins)
+            for sidx, bidx in enumerate(ids):
+                sbins[bidx-1][nidx].append(spks[sidx])
 
     return sbins
 
