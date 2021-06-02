7f9d08730196e5f450dc574cfc9a0b5e8babf3c6
chanokin <chanokin@gmail.com>
2021-06-02 18:05:26 +0100

    Adjusted single_spike_kernel_response.py row/column encodings and now it's passing.
    I also added some size comparisons, I will definitely use maximum coordinate encoding + 1 as population size

--- field_encoding/__init__.py
+++ field_encoding/__init__.py
@@ -21,7 +21,16 @@ def encode_with_field(msf, lsf, shift):
                          np.bitwise_and(lsf, mask))
 
 
-def decode_ids(ids, width, height, most_significant_rows):
+def decode_ids(ids, width=None, height=None, most_significant_rows=True, shape=None):
+    if width is not None and height is not None:
+        return decode_ids_wh(ids, width, height, most_significant_rows)
+    elif shape is not None:
+        return decode_ids_s(ids, shape, most_significant_rows)
+
+    raise Exception("Either provide width and height or shape")
+
+
+def decode_ids_wh(ids, width, height, most_significant_rows):
     size = width if most_significant_rows else height
     bits = n_bits(size)
     msb, lsb = decode_with_field(ids, bits)
@@ -30,7 +39,21 @@ def decode_ids(ids, width, height, most_significant_rows):
     return rows, cols
 
 
-def encode_coords(rows, cols, width, height, most_significant_rows):
+def decode_ids_s(ids, shape, most_significant_rows):
+    height, width = shape
+    return decode_ids_wh(ids, width, height, most_significant_rows)
+
+
+def encode_coords(rows, cols, width=None, height=None, most_significant_rows=True, shape=None):
+    if width is not None and height is not None:
+        return encode_coords_wh(rows, cols, width, height, most_significant_rows)
+    elif shape is not None:
+        return encode_coords_s(rows, cols, shape, most_significant_rows)
+
+    raise Exception("Either provide width and height or shape")
+
+
+def encode_coords_wh(rows, cols, width, height, most_significant_rows):
     size = width if most_significant_rows else height
     bits = n_bits(size)
     msb, lsb = (rows, cols) if most_significant_rows else (cols, rows)
@@ -39,7 +62,21 @@ def encode_coords(rows, cols, width, height, most_significant_rows):
     return ids
 
 
-def power_of_2_size(width, height, most_significant_rows):
+def encode_coords_s(rows, cols, shape, most_significant_rows):
+    height, width = shape
+    return encode_coords_wh(rows, cols, width, height, most_significant_rows)
+
+
+def power_of_2_size(width=None, height=None, most_significant_rows=True, shape=None):
+    if width is not None and height is not None:
+        return power_of_2_size_wh(width, height, most_significant_rows)
+    elif shape is not None:
+        return power_of_2_size_s(shape, most_significant_rows)
+
+    raise Exception("Either provide width and height or shape")
+
+
+def power_of_2_size_wh(width, height, most_significant_rows):
     msb_size = width if most_significant_rows else height
     lsb_size = height if most_significant_rows else width
     msb_bits = n_bits(msb_size)
@@ -48,9 +85,42 @@ def power_of_2_size(width, height, most_significant_rows):
     return int(2**(msb_bits + lsb_bits))
 
 
+def power_of_2_size_s(shape, most_significant_rows):
+    height, width = shape
+    return power_of_2_size_wh(width, height, most_significant_rows)
+
+
+def max_coord_size(width=None, height=None, most_significant_rows=True, shape=None):
+    if width is not None and height is not None:
+        return max_coord_size_wh(width, height, most_significant_rows)
+    elif shape is not None:
+        return max_coord_size_s(shape, most_significant_rows)
+
+    raise Exception("Either provide width and height or shape")
+
+
+def max_coord_size_wh(width, height, most_significant_rows):
+    r = height - 1
+    c = width - 1
+    return encode_coords(r, c, width, height, most_significant_rows) + 1
+
+
+def max_coord_size_s(shape, most_significant_rows):
+    height, width = shape
+    return max_coord_size_wh(width, height, most_significant_rows)
+
+
 def get_augmented_shape(shape):
     bits = n_bits(shape)
     return np.uint32(np.power(2, bits))
 
+
+def get_encoded_ids(shape, most_significant_rows):
+    n_in_rows = shape[0]
+    n_in_cols = shape[1]
+    rows = np.repeat(np.arange(n_in_rows), n_in_cols)
+    cols = np.tile(np.arange(n_in_cols), n_in_rows)
+    ids = encode_coords(rows, cols, n_in_rows, n_in_cols, most_significant_rows)
+    return ids
 
 
--- single_spike_kernel_response.py
+++ single_spike_kernel_response.py
@@ -4,10 +4,15 @@ from pyNN.space import Grid2D
 import cv2
 import matplotlib.pyplot as plt
 import sys
+import field_encoding as fe
+
+ROWS_AS_MSB = bool(1)
 
 VISUALIZE = bool(0)
 in_shape = (11, 11)
 n_input = int(np.prod(in_shape))
+n_input = fe.max_coord_size(in_shape[1], in_shape[0], ROWS_AS_MSB)
+
 stride = np.array([1, 1], dtype='int32')  # h, w
 k_shape = np.array([5, 5], dtype='int32')
 kernel = (np.arange(np.prod(k_shape)) + 1).reshape(k_shape) * 0.01
@@ -17,7 +22,10 @@ print(kernel)
 
 # sim.IF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=1024)
 # sim.IF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=2048)
-sim.NIF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=2048)
+sim.IF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=50)
+# sim.NIF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=2048)
+sim.NIF_curr_exp_conv.set_model_max_atoms_per_core(n_atoms=50)
+sim.SpikeSourceArray.set_model_max_atoms_per_core(n_atoms=50)
 # sim.SpikeSourcePoisson.set_model_max_atoms_per_core(n_atoms=1024)
 # sim.SpikeSourcePoisson.set_model_max_atoms_per_core(n_atoms=100)
 
@@ -25,9 +33,10 @@ run_time = 5.
 
 sim.setup(timestep=1.)
 
-spike_idx = ( (in_shape[0] // 2) * in_shape[1] + (in_shape[1] // 2) )
+spike_idx = fe.encode_coords((in_shape[0] // 2), (in_shape[1] // 2),
+                             in_shape[1], in_shape[0], ROWS_AS_MSB)
 spike_times = [[1.0] if i == spike_idx else []
-               for i in range(int(np.prod(in_shape)))]
+               for i in range(n_input)]
 
 src = sim.Population(n_input, sim.SpikeSourceArray,
                      {'spike_times': spike_times}, label='input spikes')
@@ -36,6 +45,7 @@ conn = sim.ConvolutionConnector(in_shape, kernel, strides=stride)
 
 out_shape = conn.get_post_shape()
 out_size = int(np.prod(out_shape))
+out_size = fe.max_coord_size(out_shape[1], out_shape[0], ROWS_AS_MSB)
 
 params = {
     'v_thresh': 1.,
@@ -67,8 +77,18 @@ vmax = kernel.max()
 print(v.shape)
 for t, vt in enumerate(v):
     img = np.zeros(out_shape)
-    img[:] = vt.reshape(out_shape)
+    # img[:] = vt.reshape(out_shape)
+    for i, v in enumerate(vt):
+        r, c = fe.decode_ids(i, most_significant_rows=ROWS_AS_MSB, shape=out_shape)
+        print(i, r, c)
+        if r >= out_shape[0] or c >= out_shape[1]:
+            continue
+
+        img[r, c] = v
+
     plt.figure()
+    ax = plt.subplot(1, 1, 1)
+    ax.set_title("Voltage at t = {}".format(t))
     im = plt.imshow(img, vmin=vmin, vmax=vmax)
     plt.colorbar(im)
 plt.show()
@@ -77,6 +97,8 @@ plt.show()
 ctr = img[1:-1, 1:-1]
 diff = kernel - ctr
 plt.figure()
+ax = plt.subplot(1, 1, 1)
+ax.set_title("Difference between output centre and kernel")
 im = plt.imshow(diff, vmin=-vmax, vmax=vmax)
 plt.colorbar(im)
 
