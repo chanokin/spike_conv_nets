3fc50ee19dd8ad51f644d0f2a62063218332ec0a
chanokin <chanokin@gmail.com>
2021-09-22 22:01:49 +0100

    input recording + classes seems to work I still see that when sending data from a SpikeSourcePoisson to a conv block, the coordinates are scrambled

--- plot_simple_cnn_mnist_from_bifrost.py
+++ plot_simple_cnn_mnist_from_bifrost.py
@@ -21,7 +21,20 @@ np_data = np.load(input_filename, allow_pickle=True)
 data_dict = to_dict(np_data)
 
 recs = data_dict['recordings']
+shapes = data_dict['shapes']
+in_cfg = data_dict['input_configuration']
+n_samples = in_cfg['num_samples']
+on_time = in_cfg['on_time_ms']
+off_time = in_cfg['off_time_ms']
+period = on_time + off_time
+run_time = period * n_samples
 for layer in recs:
     spikes = recs[layer][0].segments[0].spiketrains
-    print(layer)
-    print(spikes)
+    images = plotting.spikes_to_images(spikes, shapes[layer], run_time, period)
+    fig, axs = plt.subplots(1, n_samples, sharey=True)
+    plt.suptitle(layer)
+    for i, img in enumerate(images[0]):
+        axs[i].imshow(img)
+    plt.savefig(f"images_layer_{layer}.png", dpi=150)
+
+    # print(spikes)
