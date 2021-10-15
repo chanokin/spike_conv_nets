d9ebe1585047d4713da8110b205b6ebcdd3b779a
Garibaldi Pineda Garcia <chanokin@gmail.com>
2021-10-15 10:38:48 +0000

    trying to fix simple mnist network in norse

--- mnist_norse/mnist_norse.py
+++ mnist_norse/mnist_norse.py
@@ -5,7 +5,9 @@ import torch.utils.data
 
 from norse.torch.module.encode import PoissonEncoder
 from norse.torch.functional.lif import LIFParameters
+from norse.torch.functional.leaky_integrator import LIParameters
 from norse.torch.module.lif import LIFCell
+from norse.torch.module.leaky_integrator import LICell
 from norse.torch import SequentialState
 import pytorch_lightning as pl
 
@@ -25,47 +27,35 @@ class LIFConvNet(pl.LightningModule):
         self.lif1 = LIFCell(p=LIFParameters(method=model, alpha=100.0),)
 
         self.pool1 = torch.nn.AvgPool2d(2)
+
         self.conv2 = torch.nn.Conv2d(16, 8, 5, 1, bias=False)
         self.lif2 = LIFCell(p=LIFParameters(method=model, alpha=100.0),)
 
         self.pool2 = torch.nn.AvgPool2d(2)
 
-        self.dense1 = torch.nn.Linear(128, 64, bias=False)
+        self.dense1 = torch.nn.Linear(128, 256, bias=False)
         self.lif3 = LIFCell(p=LIFParameters(method=model, alpha=100.0),)
 
-        self.dense2 = torch.nn.Linear(64, 10, bias=False)
-        self.lif4 = LIFCell(p=LIFParameters(method=model, alpha=100.0),)
+        self.dense2 = torch.nn.Linear(256, 10, bias=False)
+        self.lif4 = LICell(p=LIFParameters(method=model, alpha=100.0),)
 
         self.seq_length = seq_length
         self.input_scale = input_scale
+        self.voltages = None
 
     def forward(self, x):
-        x = self.input_encoder(
-            x.view(-1, self.input_features) * self.input_scale
-        )
-        batch_size = x.shape[1]
-        seq_length = x.shape[0]
+        batch_size = x.shape[0]
+        seq_length = self.seq_length
+        x = self.input_encoder(x.view(-1, self.input_features))
+        x = x.reshape(seq_length, batch_size, 1, 28, 28)
 
         s0 = None
         s1 = None
         s2 = None
         s3 = None
 
-        if self.only_first_spike:
-            # delete all spikes except for first
-            zeros = torch.zeros_like(x.cpu()).detach().numpy()
-            idxs = x.cpu().nonzero().detach().numpy()
-            spike_counter = np.zeros((1, 28 * 28))
-            for t, batch, nrn in idxs:
-                if spike_counter[batch, nrn] == 0:
-                    zeros[t, batch, nrn] = 1
-                    spike_counter[batch, nrn] += 1
-            x = torch.from_numpy(zeros).to(x.device)
-
         output = []
 
-
-        x = x.reshape(seq_length, batch_size, 1, 28, 28)
         for in_step in range(seq_length):
             x_this_step = x[in_step, :]
             z = self.conv1(x_this_step)
@@ -84,9 +74,9 @@ class LIFConvNet(pl.LightningModule):
             z, s2 = self.lif3(z, s2)
 
             z = self.dense2(z)
-            z, s3 = self.lif4(z, s3)
+            z, s3 = self.lif4(torch.nn.functional.relu(z), s3)
 
-            output.append(s3.v)#.clone().detach()
+            output.append(s3.v)#.detach())
 
         # return voltages
         self.voltages = torch.stack(output)
@@ -100,7 +90,22 @@ class LIFConvNet(pl.LightningModule):
         x, y = batch
         out = self(x)
         loss = torch.nn.functional.nll_loss(out, y)
-        return loss
+
+        correct = out.argmax(dim=1).eq(y).sum().item()
+        total = len(y)
+        logs={"train_loss": loss.detach()}
+        batch_dictionary={
+            "loss": loss,
+            "log": logs,
+            "correct": correct,
+            "total": total
+        }
+
+        return batch_dictionary
+
+    def training_epoch_end(self, outputs):
+        print(outputs)
+
 
     def configure_optimizers(self):
         if self.optimizer == "adam":
